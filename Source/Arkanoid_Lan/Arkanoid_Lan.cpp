3d1805c8c5d27445db523ebb8c7ecd0e6bc7e103
XySa <Khusan.yadgarov@gmail.com>
2023-06-22 11:16:41 +0500

    git init

---
+++ Source/Arkanoid_Lan/Arkanoid_Lan.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Arkanoid_Lan.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Arkanoid_Lan, "Arkanoid_Lan" );
---
+++ Source/Arkanoid_Lan/Arkanoid_LanGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Arkanoid_LanGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKANOID_LAN_API AArkanoid_LanGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
